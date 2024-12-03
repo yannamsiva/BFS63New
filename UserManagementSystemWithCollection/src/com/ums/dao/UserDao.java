40b2ba5c81809fcb2e28ffafdca8e2e5a5295ca6
yannamsiva <46184940+yannamsiva@users.noreply.github.com>
2024-12-03 21:21:06 +0530

    Delete UserManagementSystemWithCollection directory

--- UserManagementSystemWithCollection/src/com/ums/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.ums.dao;
-
-public interface UserDao {
-public void register();
-public boolean verifyUserNameAndPassWord(String uname,String pass);
-String getPassWord(String email);
-}
